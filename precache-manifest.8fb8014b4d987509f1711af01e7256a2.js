0a447a7b08dbd5c0a0582afd2c131ea6ebd837ca
Andrew Lee <Andrew@Andrews-MacBook-Pro.local>
2020-02-15 07:04:38 -0800

    Updates

---
+++ precache-manifest.8fb8014b4d987509f1711af01e7256a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00456fd8017d5c5980447d17c6ca4de0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "694ac2d62d723cba6974",
+    "url": "/monsters-rolodex/static/css/main.d1d31914.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "694ac2d62d723cba6974",
+    "url": "/monsters-rolodex/static/js/main.943481db.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
